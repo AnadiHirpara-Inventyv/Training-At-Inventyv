af8382e7a23e1df6f5221ba5a9ab805b3cd79de3
defcarrear <defcarrear2024@gmail.com>
2023-12-31 14:10:05 +0530

    My Initial Commit for Inventyv

---
+++ Task_0 Logic Building/Flowchart_1/Area_Circle.java
@@ -0,0 +1,22 @@
+package Flowchart_1;
+
+import java.util.Scanner;
+
+/**
+ * Area_Circle
+ */
+public class Area_Circle {
+
+      public static void main(String[] args) 
+    {
+        int r;
+        double pi = 3.14, area;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter radius of circle: ");
+        r = sc.nextInt();
+        area = pi * r * r;
+        System.out.println("Area of circle: "+area);
+        sc.close();
+    } 
+    
+}
