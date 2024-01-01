efd36e151854a93620c6c4e9bcf0927903b67965
Anadi Hirpara <anadihirparainventyv@gmail.com>
2024-01-01 10:42:24 +0530

    Area of Circle

--- Task_0 Logic Building/Flowchart_1/Area_Circle.java
+++ Task_0 Logic Building/Flowchart_1/Area_Circle.java
@@ -16,7 +16,7 @@ public class Area_Circle {
         System.out.print("Enter radius of circle: ");
         r = sc.nextInt();
         area = pi * r * r;
-        System.out.println("Area of circle: "+area);
+        System.out.println("Area of circle: "+area + "m²");
         sc.close();
     } 
     
