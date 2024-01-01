e1db5977da6c1121edd7e2b61f3a0512515bf45c
Anadi Hirpara <anadihirparainventyv@gmail.com>
2024-01-01 10:37:24 +0530

    this commit for flowchart 3

---
+++ Task_0 Logic Building/Flowchart_3/Age_Validation.java
@@ -0,0 +1,19 @@
+package Flowchart_3;
+
+import java.util.Scanner;
+
+public class Age_Validation {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+    
+    System.out.print("Read Age: ");
+    int age = scanner.nextInt();
+    
+    if (age >= 21) {
+      System.out.println("Adult");
+    } else {
+      System.out.println("Not Adult");
+    }
+    }
+    
+}
