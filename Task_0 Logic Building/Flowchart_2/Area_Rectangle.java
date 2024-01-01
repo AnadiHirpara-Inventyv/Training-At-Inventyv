e5c4c567859043b807e8bb8d2d326ee8597598a8
Anadi Hirpara <anadihirparainventyv@gmail.com>
2024-01-01 10:32:49 +0530

    commit for Flowchart 2

---
+++ Task_0 Logic Building/Flowchart_2/Area_Rectangle.java
@@ -0,0 +1,25 @@
+package Flowchart_2;
+
+import java.util.Scanner;
+
+public class Area_Rectangle {
+    public static void main(String[] args) {
+        
+         Scanner scanner = new Scanner(System.in);
+        
+        int l, b, A, P;
+        
+        System.out.print("Enter length (l): ");
+        l = scanner.nextInt();
+        
+        System.out.print("Enter breadth (b): ");
+        b = scanner.nextInt();
+        
+        A = l * b;
+        System.out.println("Area: " + A);
+        
+        P = 2 * (l + b);
+        System.out.println("Perimeter: " + P);
+    }
+    
+}
